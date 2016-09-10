e33a22f52e5dd907954f5814775f1c28903912d4
Razican <razican@protonmail.ch>
2016-09-10 16:03:43 -0400

    Added PendingTransactionDTO

--- src/v1/transactions.rs
+++ src/v1/transactions.rs
@@ -5,36 +5,45 @@ use utils::{WalletAddress, Amount};
 
 use DTO;
 
-/// Struct used to generate a transaction
+/// Struct used to generate a transaction.
 #[derive(Clone, RustcEncodable, RustcDecodable)]
 pub struct GenerateTransactionDTO {
-    /// Where the transaction originated
+    /// Where the transaction originated.
     pub origin_id: u64,
-    /// The destination wallet address of the transaction
+    /// The destination wallet address of the transaction.
     pub destination_address: WalletAddress,
-    /// Who the transaction is going to
+    /// Who the transaction is going to.
     pub destination_id: u64,
-    /// The amount of the transaction
+    /// The amount of the transaction.
     pub amount: Amount,
 }
 
 impl DTO for GenerateTransactionDTO {}
 
-/// Struct for tansactions
+/// Struct for tansactions.
 #[derive(Clone, RustcEncodable, RustcDecodable)]
 pub struct TransactionDTO {
-    /// The id of the transaction
+    /// The id of the transaction.
     pub id: u64,
-    /// The origin of the transaction
+    /// The origin of the transaction.
     pub origin_user: u64,
-    /// The destination of the transaction
+    /// The destination of the transaction.
     pub destination_user: u64,
-    /// The destination address of the transaction
+    /// The destination address of the transaction.
     pub destination_address: WalletAddress,
-    /// The amount of the transaction
+    /// The amount of the transaction.
     pub amount: Amount,
-    /// The timestamp of the transaction
+    /// The timestamp of the transaction.
     pub timestamp: DateTime<UTC>,
 }
 
 impl DTO for TransactionDTO {}
+
+/// Struct for pending transactions.
+#[derive(Clone, RustcEncodable, RustcDecodable)]
+pub struct PendingTransactionDTO {
+    /// The pending transaction code.
+    pub code: String,
+}
+
+impl DTO for PendingTransactionDTO {}
