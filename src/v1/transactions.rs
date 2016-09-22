dfe8ff788e401895cdb5506677d64e166fa4abd2
Eric Remigino <eric.remigino@gmail.com>
2016-09-22 13:05:22 -0400

    removed unused dto

--- src/v1/transactions.rs
+++ src/v1/transactions.rs
@@ -47,14 +47,3 @@ pub struct PendingTransactionDTO {
 }
 
 impl DTO for PendingTransactionDTO {}
-
-/// Struct for Authenticating transactions.
-#[derive(Clone, RustcEncodable, RustcDecodable)]
-pub struct AuthenticateTransactionDTO {
-    /// The pending transaction key.
-    pub key: String,
-    /// The 6 digit 2 factor authentication code.
-    pub code: u32,
-}
-
-impl DTO for AuthenticateTransactionDTO {}
